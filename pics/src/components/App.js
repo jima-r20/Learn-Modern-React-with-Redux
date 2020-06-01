c1d74be2d04ec32b11c0abfb69ed2832b9469855
ryuji <ryu.seven.h7420@gmail.com>
2020-06-02 07:47:36 +0900

    section8_02

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
-      headers: {
-        Authorization: 'Client-ID 8U_2et1eRqFZ12yvlKF7tsTMIl-_TioGmlF6zizJcUI',
-      },
     });
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
