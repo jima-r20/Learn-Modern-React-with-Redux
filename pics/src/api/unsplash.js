6f4eb879b5376dcbb89011b0a70ee2b6f75dfcd7
ryuji <ryu.seven.h7420@gmail.com>
2020-06-03 08:23:47 +0900

    section11_01

--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID 8U_2et1eRqFZ12yvlKF7tsTMIl-_TioGmlF6zizJcUI',
-  },
-});
