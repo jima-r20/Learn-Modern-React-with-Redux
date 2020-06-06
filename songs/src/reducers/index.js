3b0b1526c4ed3702c1afa5c5ee4becfebcb8702c
ryuji <ryu.seven.h7420@gmail.com>
2020-06-06 19:53:43 +0900

    section13_02

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { selectSong } from '../actions';
+
 const songsReducer = () => {
   return [
     { title: 'music1', duration: '5:15' },
@@ -5,4 +7,12 @@ const songsReducer = () => {
     { title: 'music3', duration: '3:54' },
     { title: 'music4', duration: '2:22' },
   ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectSong;
 };
