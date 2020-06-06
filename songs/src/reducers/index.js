768e4a1710ea9ca2c690de590e8ea685aa75de41
ryuji <ryu.seven.h7420@gmail.com>
2020-06-06 19:53:16 +0900

    section13_02

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: 'music1', duration: '5:15' },
+    { title: 'music2', duration: '4:20' },
+    { title: 'music3', duration: '3:54' },
+    { title: 'music4', duration: '2:22' },
+  ];
+};
