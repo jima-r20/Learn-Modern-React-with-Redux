d51c38de07f1f77573d8885fa4bc02fe2dedfa24
ryuji <ryu.seven.h7420@gmail.com>
2020-06-21 18:39:41 +0900

    section24_03

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,15 +3,15 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : '送信';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : '送信';
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
